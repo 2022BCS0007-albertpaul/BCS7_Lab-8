be92f3858f8a85373d959c05aede5f2967bc7b30
2022BCS0007-albertpaul <alberts@hotmail.com>
2026-03-06 12:02:31 +0530

    Updated GitHub Actions workflow

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import json
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 
 # Load dataset
 
@@ -38,7 +39,8 @@ y_pred = model.predict(X_test)
 
 # Metrics
 
-rmse = mean_squared_error(y_test, y_pred, squared=False)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
 r2 = r2_score(y_test, y_pred)
 
 metrics = {
